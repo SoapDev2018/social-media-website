db7c79d66750f55a347ef241714426c2a44a7c10
Sayantan Chaudhuri <sayantandev2018@gmail.com>
2019-03-27 08:31:47 +0530

    Fixed time difference

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -61,67 +61,68 @@
         $profile_pic = $user_row['profile_pic'];
 
         //Getting timeframe
-        $date_object = new DateTime();
-        $date_time_now = $date_object->format('Y-m-d H:i:s');
-        $start_date = new DateTime($date_time);
-        $end_time = new DateTime($date_time_now);
-        $interval = $start_date->diff($end_time);
-        if($interval->y >= 1) {
-          if($interval->y == 1)
-            $time_message = $interval->y . " year ago";
-          else
-            $time_message = $interval->y . " years ago";
-        }
-        else if($interval->m >= 1) {
-          if($interval->d == 0) {
-            $days = " ago";
-          }
-          else if($interval->d == 1) {
-            $days = $interval->d . " day ago";
-          }
-          else {
-            $days = $interval->d . " days ago";
-          }
+        $date_time_now = date("Y-m-d H:i:s");
+					$start_date = new DateTime($date_time); //Time of post
+					$end_date = new DateTime($date_time_now); //Current time
+					$interval = $start_date->diff($end_date); //Difference between dates 
+					if($interval->y >= 1) {
+						if($interval == 1)
+							$time_message = $interval->y . " year ago"; //1 year ago
+						else 
+							$time_message = $interval->y . " years ago"; //1+ year ago
+					}
+					else if ($interval-> m >= 1) {
+						if($interval->d == 0) {
+							$days = " ago";
+						}
+						else if($interval->d == 1) {
+							$days = $interval->d . " day ago";
+						}
+						else {
+							$days = $interval->d . " days ago";
+						}
 
-          if($interval->m == 1) {
-            $time_message = $interval->m . " month" . $days;
-          }else {
-            $time_message = $interval->m . " months" . $days;
-          }
-        }
-        else if($interval->d >= 1) {
-          if($interval->d == 1) {
-            $time_message = "Yesterday";
-          }
-          else {
-            $time_message = $interval->d . " days ago";
-          }
-        }
-        else if($interval->h == 1) {
-          if($interval->h == 1) {
-            $time_message = $interval->h . " hour ago";
-          }
-          else {
-            $time_message = $interval->h . " hours ago";
-          }
-        }
-        else if($interval->i == 1) {
-          if($interval->i == 1) {
-            $time_message = $interval->i . " minute ago";
-          }
-          else {
-            $time_message = $interval->i . " minutes ago";
-          }
-        }
-        else {
-          if($interval->s < 30) {
-            $time_message = "Just now";
-          }
-          else {
-            $time_message = $interval->s . " seconds ago";
-          }
-        }
 
+						if($interval->m == 1) {
+							$time_message = $interval->m . " month". $days;
+						}
+						else {
+							$time_message = $interval->m . " months". $days;
+						}
+					}
+				  else if($interval->d >= 1) {
+						if($interval->d == 1) {
+							$time_message = "Yesterday";
+						}
+						else {
+							$time_message = $interval->d . " days ago";
+						}
+					}
+					else if($interval->h >= 1) {
+						if($interval->h == 1) {
+							$time_message = $interval->h . " hour ago";
+						}
+						else {
+							$time_message = $interval->h . " hours ago";
+						}
+					}
+					else if($interval->i >= 1) {
+						if($interval->i == 1) {
+							$time_message = $interval->i . " minute ago";
+						}
+						else {
+							$time_message = $interval->i . " minutes ago";
+						}
+					}
+					else {
+						if($interval->s < 30) {
+							$time_message = "Just now";
+						}
+						else {
+							$time_message = $interval->s . " seconds ago";
+						}
+          }
+          
         $str .= "<div class='status_post'>
                   <div class='post_profile_pic'>
                     <img src='$profile_pic' width='50'>
