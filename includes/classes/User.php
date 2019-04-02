4703b43146fbe9a2493b7ec01bede1f301bf7e6e
Sayantan Chaudhuri <sayantandev2018@gmail.com>
2019-04-02 22:39:08 +0530

    Implemented add friend functionality

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -55,8 +55,8 @@
       }
     }
 
-    public function didReceiveRequest($user_to) {
-      $user_from = $this->user['username'];
+    public function didReceiveRequest($user_from) {
+      $user_to = $this->user['username'];
       $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
       if(mysqli_num_rows($check_request_query) > 0)
         return true;
@@ -64,8 +64,8 @@
         return false;
     }
 
-    public function didSendRequest($user_from) {
-      $user_to = $this->user['username'];
+    public function didSendRequest($user_to) {
+      $user_from = $this->user['username'];
       $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
       if(mysqli_num_rows($check_request_query) > 0)
         return true;
@@ -84,6 +84,11 @@
       
       $new_friend_array = str_replace($this->user['username'] . ",","",$friend_array_username);
       $remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
+    }
+
+    public function sendRequest($user_to) {
+      $user_from = $this->user['username'];
+      $query = mysqli_query($this->con, "INSERT INTO friend_requests VALUES('','$user_to','$user_from')");
     }
   }
 ?>
--- profile.php
+++ profile.php
@@ -5,9 +5,20 @@ include("includes/classes/Post.php");
 if(isset($_GET['profile_username'])) {
     $username = $_GET['profile_username'];
     $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
-    $user_arrar = mysqli_fetch_array($user_details_query);
-    $num_friends = (substr_count($user_arrar['friend_array'],","))-1;
+    $user_array = mysqli_fetch_array($user_details_query);
+    $num_friends = (substr_count($user_array['friend_array'],","))-1;
 }
+
+if(isset($_POST['remove_friend'])) {
+    $user = new User($con, $userLoggedIn);
+    $user->removeFriend($username);
+}
+if(isset($_POST['add_friend'])) {
+    $user = new User($con, $userLoggedIn);
+    $user->sendRequest($username);
+}
+if(isset($_POST['respond_request']))
+    header("Location: requests.php");
 ?>
     <style>
         .wrapper {
@@ -16,13 +27,13 @@ if(isset($_GET['profile_username'])) {
         }
     </style>
     <div class="profile_left">
-        <img src="<?php echo $user_arrar['profile_pic']; ?>">
+        <img src="<?php echo $user_array['profile_pic']; ?>">
         <div class="profile_info">
-            <p><?php echo "Posts: " . $user_arrar['num_posts']; ?></p>
-            <p><?php echo "Likes: " . $user_arrar['num_likes']; ?></p>
+            <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+            <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
             <p><?php echo "Friends: " . $num_friends; ?></p>
         </div>
-        <form action="<?php echo $username; ?>">
+        <form action="<?php echo $username; ?>" method="POST">
             <?php $profile_user_obj = new User($con, $username);
                 if($profile_user_obj->isClosed())
                     header("Location: user_closed.php");
@@ -33,7 +44,7 @@ if(isset($_GET['profile_username'])) {
                     else if($logged_in_user_obj->didReceiveRequest($username))
                         echo '<input type="submit" name="respond_request" class="warning btn btn-warning" value="Respond to Request"><br>';
                     else if($logged_in_user_obj->didSendRequest($username))
-                        echo '<input type="submit" name="" class="default btn btn-secondary" value="Request Sent!"><br>';
+                        echo '<input type="submit" class="default btn btn-secondary" value="Request Sent!" disabled><br>';
                     else
                         echo '<input type="submit" name="add_friend" class="success btn btn-success" value="Add Friend"><br>';
                 }
